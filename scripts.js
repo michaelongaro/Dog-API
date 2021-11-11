eac8555e5788372bad7f4e6bf9d7f6d4a02d0757
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2021-11-11 17:55:38 -0600

    added dynamic creation of weather "cards" and removed unnecessary helper functions

--- scripts.js
+++ scripts.js
@@ -15,6 +15,96 @@ const saturday = document.getElementById("saturday")
 const sunday = document.getElementById("sunday")
 
 const days = [sunday, monday, tuesday, wednesday, thursday, friday, saturday]
+const days_str = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+function setupCardStructure() {
+    for (const i in days) {
+        // create parent and two child divs for day of week and weather icon
+        const day_icon_container = document.createElement("div");
+        day_icon_container.setAttribute("id", "day-icon-container");
+
+        const day_of_week = document.createElement("div");
+        day_of_week.setAttribute("id", "day-of-week");
+
+        const weather_status_icon = document.createElement("div");
+        weather_status_icon.setAttribute("id", "weather-status-icon");
+
+        // appending children divs to parent
+        day_icon_container.append(day_of_week);
+        day_icon_container.append(weather_status_icon);
+
+        // create both high and low temp divs
+        const high_container = document.createElement("div");
+        high_container.setAttribute("id", "high-container");
+
+        const low_container = document.createElement("div");
+        low_container.setAttribute("id", "low-container");
+
+        // create divs for the high/low labels
+        const high_label = document.createElement("div");
+        high_label.setAttribute("id", "high-label");
+
+        const low_label = document.createElement("div");
+        low_label.setAttribute("id", "low-label");
+        
+        // create divs for the actual temperatures
+        const high_temperature = document.createElement("div");
+        high_temperature.setAttribute("id", "high-temperature");
+
+        const low_temperature = document.createElement("div");
+        low_temperature.setAttribute("id", "low-temperature");
+
+        // appending both labels and temps to their parent divs
+        high_container.append(high_label);
+        high_container.append(high_temperature);
+
+        low_container.append(low_label);
+        low_container.append(low_temperature);
+
+        // add day of week + 'high' and 'low' text
+        day_of_week.innerHTML = days_str[i];
+        high_label.innerHTML = "High";
+        low_label.innerHTML = "Low";
+
+        days[i].append(day_icon_container);
+        days[i].append(high_container);
+        days[i].append(low_container);
+    }
+}
+
+
+const fonts = [
+    "Arial, sans-serif",
+    "Helvetica, sans-serif",
+    "Gill Sans, sans-serif",
+    "Lucida, sans-serif",
+    "Helvetica Narrow, sans-serif",
+    "sans-serif",
+    "Times, serif",
+    "Times New Roman, serif",
+    "Palatino, serif",
+    "Bookman, serif",
+    "New Century Schoolbook, serif",
+    "serif",
+    "Andale Mono, monospace",
+    "Courier New, monospace",
+    "Courier, monospace",
+    "Lucidatypewriter, monospace",
+    "Fixed, monospace",
+    "monospace",
+    "Comic Sans, Comic Sans MS, cursive",
+    "Zapf Chancery, cursive",
+    "Coronetscript, cursive",
+    "Florence, cursive",
+    "Parkavenue, cursive",
+    "cursive",
+    "Impact, fantasy",
+    "Arnoldboecklin, fantasy",
+    "Oldtown, fantasy",
+    "Blippo, fantasy",
+    "Brushstroke, fantasy",
+    "fantasy"
+]
 
 const d = new Date();
 let current_day = d.getDay()
@@ -26,7 +116,7 @@ let autofill_str = "";
 
 window.addEventListener("keyup", function (event) {
 
-    if ((event.which >= 65 && event.which <= 90) || (event.code === "Enter" || event.code === "Backspace")) {
+    if ((event.which >= 65 && event.which <= 90) || event.code === "Backspace") {
         clearAutofillResults();
         autofill_str = search.value;
         if (autofill_str.length != 0) {
@@ -58,35 +148,20 @@ function shift_days() {
     return current_forcast_days;
 }
 
-
-function showForcast(element) {
-    element.style.display = "flex";
-}
-
-// hides days that are not part of forcast
-function hideExtraDays() {
-    
-    for (const day of current_forcast_days) {
-        if (!day.hasChildNodes()) {
-            day.style.display = "none";
-        }
-    }
-}
-
 function showCityName(city) {
-    cityName.innerHTML = city
+    // resets current cityName value
+    cityName.innerHTML = "";
+
+    cityName.innerHTML = city;
     cityName.style.display = "block";
-    randomizeFont(cityName)
+    randomizeFont(cityName);
 }
 
 function randomizeFont(element) {
-    
+    console.log(fonts[Math.floor(Math.random() * fonts.length)]);
+    element.style.fontFamily = fonts[Math.floor(Math.random() * fonts.length)];
+    element.style.fontSize = "30pt";
 }
-
-function formatCityString(unformatted_str) {
-    return unformatted_str.replaceAll(' ', '%20')
-}
-
 
 // ideally would like to filter by results that are in same country as user, how to ask without 
 // making them say yes on the popup
@@ -125,36 +200,38 @@ async function renderAutofillResults() {
         res_list.append(temp_div);
 
         temp_div.addEventListener("click", function (event) {
+            showCityName(result["LocalizedName"]);
             renderWeather(result["Key"]);
         }, true);
 
+        temp_div.addEventListener("keyup", function (event) {
+            if (event.code === "Enter") {
+                event.preventDefault();
+                temp_div.click();
+            }
+        }, true);
 
     autofill_str = "";
+    }
 }
 
 async function renderWeather(city_loc) {
     let raw_days = await getWeather(city_loc);
+    setupCardStructure();
     shift_days();
-    hideExtraDays();
     clearAutofillResults();
     days_arr = raw_days["DailyForecasts"];
     console.log(days_arr);
     
     for (let i = 0; i < 5; i++) {
-        const max_temp = document.createElement("div");
-        max_temp.innerHTML = days_arr[i]["Temperature"]["Maximum"]["Value"];
-        const min_temp = document.createElement("div");
-        min_temp.innerHTML = days_arr[i]["Temperature"]["Minimum"]["Value"];
-        current_forcast_days[i].append(max_temp);
-        current_forcast_days[i].append(min_temp);
-
-
+        console.log(current_forcast_days[i].children[1]);
+        current_forcast_days[i].children[1].children[1].innerHTML = days_arr[i]["Temperature"]["Maximum"]["Value"];
+        current_forcast_days[i].children[2].children[1].innerHTML = days_arr[i]["Temperature"]["Minimum"]["Value"];
+        
+        // ok to show that day since the parent div is still hidden
+        current_forcast_days[i].style.display = "flex";
     }
 
-    // console.log(days_arr);
-    
     container.style.display = "flex";
-    document.querySelectorAll('.day').forEach(showForcast);
-
 }
 
