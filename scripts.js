0dea5f7d869aafa9f161e316ebdfaaa4f203cb4b
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2021-11-18 21:25:44 -0600

    added proper "cards" in forcast, cleaned up ui

--- scripts.js
+++ scripts.js
@@ -14,8 +14,14 @@ const friday = document.getElementById("friday")
 const saturday = document.getElementById("saturday")
 const sunday = document.getElementById("sunday")
 
+let weather_status_icon;
+let high_temperature;
+let low_temperature;
+
 const days = [sunday, monday, tuesday, wednesday, thursday, friday, saturday]
 const days_str = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+let cards_created = false;
 
 function setupCardStructure() {
     for (const i in days) {
@@ -26,8 +32,9 @@ function setupCardStructure() {
         const day_of_week = document.createElement("div");
         day_of_week.setAttribute("id", "day-of-week");
 
-        const weather_status_icon = document.createElement("div");
+        weather_status_icon = document.createElement("div");
         weather_status_icon.setAttribute("id", "weather-status-icon");
+        weather_status_icon.setAttribute("class", "loading");
 
         // appending children divs to parent
         day_icon_container.append(day_of_week);
@@ -48,11 +55,13 @@ function setupCardStructure() {
         low_label.setAttribute("id", "low-label");
         
         // create divs for the actual temperatures
-        const high_temperature = document.createElement("div");
+        high_temperature = document.createElement("div");
         high_temperature.setAttribute("id", "high-temperature");
+        high_temperature.setAttribute("class", "loading");
 
-        const low_temperature = document.createElement("div");
+        low_temperature = document.createElement("div");
         low_temperature.setAttribute("id", "low-temperature");
+        low_temperature.setAttribute("class", "loading");
 
         // appending both labels and temps to their parent divs
         high_container.append(high_label);
@@ -70,6 +79,8 @@ function setupCardStructure() {
         days[i].append(high_container);
         days[i].append(low_container);
     }
+
+    cards_created = true;
 }
 
 
@@ -132,16 +143,18 @@ function clearAutofillResults() {
     }
 }
 
+function clearSearchBar() {
+    search.value = "";
+}
+
 function shift_days() {
     counter = 0;
     while (counter < 5) {
         if (current_day + counter > 6) {
             current_forcast_days.push(days[(current_day + counter) % 7]);
-            console.log((current_day + counter) % 6);
         }
         else {
             current_forcast_days.push(days[current_day + counter]);
-            console.log((current_day + counter));
         }
         counter += 1;
     }
@@ -149,7 +162,7 @@ function shift_days() {
 }
 
 function showCityName(city) {
-    // resets current cityName value
+
     cityName.innerHTML = "";
 
     cityName.innerHTML = city;
@@ -163,9 +176,6 @@ function randomizeFont(element) {
     element.style.fontSize = "30pt";
 }
 
-// ideally would like to filter by results that are in same country as user, how to ask without 
-// making them say yes on the popup
-//                            ^^^^^
 async function getAutofillResults(str) {
     let autofill_url = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=5A30FxiSCT46fzC7G35geCxlL0Xeuqwp&q=${str}`
 
@@ -178,6 +188,7 @@ async function getAutofillResults(str) {
     }
 
 }
+
 
 async function getWeather(city_loc) {
     let forcast_url = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${city_loc}?apikey=5A30FxiSCT46fzC7G35geCxlL0Xeuqwp`;
@@ -193,14 +204,13 @@ async function getWeather(city_loc) {
 
 async function renderAutofillResults() {
     let results = await getAutofillResults(autofill_str);
-
     for (const result of results) {
         let temp_div = document.createElement("div");
-        temp_div.innerHTML = `${result["LocalizedName"]}`;
+        temp_div.innerHTML = `${result["LocalizedName"]}, ${result["AdministrativeArea"]["ID"]}`;
         res_list.append(temp_div);
 
         temp_div.addEventListener("click", function (event) {
-            showCityName(result["LocalizedName"]);
+            showCityName(`${result["LocalizedName"]}, ${result["AdministrativeArea"]["ID"]}`);
             renderWeather(result["Key"]);
         }, true);
 
@@ -217,21 +227,39 @@ async function renderAutofillResults() {
 
 async function renderWeather(city_loc) {
     let raw_days = await getWeather(city_loc);
-    setupCardStructure();
-    shift_days();
-    clearAutofillResults();
-    days_arr = raw_days["DailyForecasts"];
-    console.log(days_arr);
-    
-    for (let i = 0; i < 5; i++) {
-        console.log(current_forcast_days[i].children[1]);
-        current_forcast_days[i].children[1].children[1].innerHTML = days_arr[i]["Temperature"]["Maximum"]["Value"];
-        current_forcast_days[i].children[2].children[1].innerHTML = days_arr[i]["Temperature"]["Minimum"]["Value"];
-        
-        // ok to show that day since the parent div is still hidden
-        current_forcast_days[i].style.display = "flex";
+    console.log(raw_days);
+    if (!cards_created) {
+        setupCardStructure();
+        shift_days();
     }
-
+    clearAutofillResults();
+    clearSearchBar();
+    days_arr = raw_days["DailyForecasts"];
     container.style.display = "flex";
+
+    for (let k = 0; k < 5; k++) {
+        current_forcast_days[k].style.display = "flex";
+    }
+    
+    // ideally want to also include the wind values + change the angle of the background gradient to match the direction of the wind
+
+    setTimeout(() => {
+        for (let i = 0; i < 5; i++) {
+            current_forcast_days[i].children[0].children[1].classList.remove("loading");
+            current_forcast_days[i].children[1].children[1].classList.remove("loading");
+            current_forcast_days[i].children[2].children[1].classList.remove("loading");
+        }
+    }, 3000);
+
+    setTimeout(() => {
+        for (let j = 0; j < 5; j++) {
+            current_forcast_days[j].children[0].children[1].innerHTML = `<img src='./weather-images/${days_arr[j]["Day"]["Icon"]}-s.png'>`
+            current_forcast_days[j].children[1].children[1].innerHTML = days_arr[j]["Temperature"]["Maximum"]["Value"];
+            current_forcast_days[j].children[2].children[1].innerHTML = days_arr[j]["Temperature"]["Minimum"]["Value"];
+        }
+    }, 3001);
+
+
+    
 }
 
