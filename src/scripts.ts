e9575db2b9ccf3de9e3839db9edc14aba70c2eab
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-09-24 03:08:18 -0500

    made gradient a little more vibrant

--- src/scripts.ts
+++ src/scripts.ts
@@ -496,91 +496,91 @@ function calculateBackgroundGradientByTemps(
   let coldestHSLValue, hottestHSLValue;
 
   if (min_weekly_temp <= -40) {
-    coldestHSLValue = "hsl(240 100% 35%)";
+    coldestHSLValue = "hsl(240 100% 30%)";
   } else if (min_weekly_temp > -40 && min_weekly_temp <= 0) {
-    coldestHSLValue = "hsl(240 100% 45%)";
+    coldestHSLValue = "hsl(240 100% 40%)";
   } else if (min_weekly_temp > 0 && min_weekly_temp <= 30) {
-    coldestHSLValue = "hsl(240 100% 65%)";
+    coldestHSLValue = "hsl(240 100% 60%)";
   } else if (min_weekly_temp > 30 && min_weekly_temp <= 50) {
     coldestHSLValue = `hsl(${mapTempsToColorRange(
       min_weekly_temp,
       [30, 50],
       [240, 180]
-    )} 100% 65%)`;
+    )} 100% 60%)`;
   } else if (min_weekly_temp > 50 && min_weekly_temp <= 70) {
     coldestHSLValue = `hsl(${mapTempsToColorRange(
       min_weekly_temp,
       [50, 70],
       [60, 55]
-    )} 100% 65%)`;
+    )} 100% 60%)`;
   } else if (min_weekly_temp > 70 && min_weekly_temp <= 80) {
     coldestHSLValue = `hsl(${mapTempsToColorRange(
       min_weekly_temp,
       [70, 80],
       [55, 40]
-    )} 100% 65%)`;
+    )} 100% 60%)`;
   } else if (min_weekly_temp > 80 && min_weekly_temp <= 90) {
     coldestHSLValue = `hsl(${mapTempsToColorRange(
       min_weekly_temp,
       [80, 90],
       [40, 20]
-    )} 100% 65%)`;
+    )} 100% 60%)`;
   } else if (min_weekly_temp > 90 && min_weekly_temp <= 100) {
     coldestHSLValue = `hsl(${mapTempsToColorRange(
       min_weekly_temp,
       [90, 100],
       [20, 10]
-    )} 100% 65%)`;
+    )} 100% 60%)`;
   } else if (min_weekly_temp > 100) {
     coldestHSLValue = `hsl(${mapTempsToColorRange(
       min_weekly_temp,
       [100, 130],
       [10, 0]
-    )} 100% 65%)`;
+    )} 100% 60%)`;
   }
 
   if (max_weekly_temp <= -40) {
-    hottestHSLValue = "hsl(240 100% 35%)";
+    hottestHSLValue = "hsl(240 100% 30%)";
   } else if (max_weekly_temp > -40 && max_weekly_temp <= 0) {
-    hottestHSLValue = "hsl(240 100% 45%)";
+    hottestHSLValue = "hsl(240 100% 40%)";
   } else if (max_weekly_temp > 0 && max_weekly_temp <= 30) {
-    hottestHSLValue = "hsl(240 100% 65%)";
+    hottestHSLValue = "hsl(240 100% 60%)";
   } else if (max_weekly_temp > 30 && max_weekly_temp <= 50) {
     hottestHSLValue = `hsl(${mapTempsToColorRange(
       max_weekly_temp,
       [30, 50],
       [240, 180]
-    )} 100% 65%)`;
+    )} 100% 60%)`;
   } else if (max_weekly_temp > 50 && max_weekly_temp <= 70) {
     hottestHSLValue = `hsl(${mapTempsToColorRange(
       max_weekly_temp,
       [50, 70],
       [60, 55]
-    )} 100% 65%)`;
+    )} 100% 60%)`;
   } else if (max_weekly_temp > 70 && max_weekly_temp <= 80) {
     hottestHSLValue = `hsl(${mapTempsToColorRange(
       max_weekly_temp,
       [70, 80],
       [55, 40]
-    )} 100% 65%)`;
+    )} 100% 60%)`;
   } else if (max_weekly_temp > 80 && max_weekly_temp <= 90) {
     hottestHSLValue = `hsl(${mapTempsToColorRange(
       max_weekly_temp,
       [80, 90],
       [40, 20]
-    )} 100% 65%)`;
+    )} 100% 60%)`;
   } else if (max_weekly_temp > 90 && max_weekly_temp <= 100) {
     hottestHSLValue = `hsl(${mapTempsToColorRange(
       max_weekly_temp,
       [90, 100],
       [20, 10]
-    )} 100% 65%)`;
+    )} 100% 60%)`;
   } else if (max_weekly_temp > 100) {
     hottestHSLValue = `hsl(${mapTempsToColorRange(
       max_weekly_temp,
       [100, 130],
       [10, 0]
-    )} 100% 65%)`;
+    )} 100% 60%)`;
   }
 
   document.documentElement.style.setProperty(
