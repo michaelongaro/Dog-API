ca46123d688740bfb4db11ef541cd5ecbc2c755f
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-09-26 20:16:46 -0500

    imported dynamic location paths for anim. svgs

--- src/util/weather_icons.ts
+++ src/util/weather_icons.ts
@@ -1,44 +1,61 @@
+import day from "../../assets/day.svg";
+import lightDayClouds from "../../assets/lightDayClouds.svg";
+import mediumDayClouds from "../../assets/mediumDayClouds.svg";
+import heavyDayClouds from "../../assets/heavyDayClouds.svg";
+import lightRain from "../../assets/lightRain.svg";
+import rainWithSun from "../../assets/rainWithSun.svg";
+import thunder from "../../assets/thunder.svg";
+import heavyRain from "../../assets/heavyRain.svg";
+import lightSnow from "../../assets/lightSnow.svg";
+import heavySnow from "../../assets/heavySnow.svg";
+import snowWithSun from "../../assets/snowWithSun.svg";
+import mediumSnow from "../../assets/mediumSnow.svg";
+import night from "../../assets/night.svg";
+import lightNightClouds from "../../assets/lightNightClouds.svg";
+import mediumNightClouds from "../../assets/mediumNightClouds.svg";
+import heavyNightClouds from "../../assets/heavyNightClouds.svg";
+
 export const weather_icons = [
-  "assets/day.svg",
-  "assets/day.svg",
-  "assets/lightDayClouds.svg",
-  "assets/mediumDayClouds.svg",
-  "assets/mediumDayClouds.svg",
-  "assets/heavyDayClouds.svg",
-  "assets/heavyDayClouds.svg",
-  "assets/heavyDayClouds.svg",
-  "assets/heavyDayClouds.svg",
-  "assets/lightRain.svg",
-  "assets/rainWithSun.svg",
-  "assets/rainWithSun.svg",
-  "assets/thunder.svg",
-  "assets/rainWithSun.svg",
-  "assets/rainWithSun.svg",
-  "assets/heavyRain.svg",
-  "assets/lightSnow.svg",
-  "assets/lightSnow.svg",
-  "assets/lightSnow.svg",
-  "assets/heavySnow.svg",
-  "assets/snowWithSun.svg",
-  "assets/mediumSnow.svg",
-  "assets/mediumSnow.svg",
-  "assets/mediumSnow.svg",
-  "assets/mediumSnow.svg",
-  "assets/mediumSnow.svg",
-  "assets/mediumSnow.svg",
-  "assets/day.svg",
-  "assets/day.svg",
-  "assets/lightDayClouds.svg",
-  "assets/night.svg",
-  "assets/lightNightClouds.svg",
-  "assets/lightNightClouds.svg",
-  "assets/lightNightClouds.svg",
-  "assets/mediumNightClouds.svg",
-  "assets/heavyNightClouds.svg",
-  "assets/mediumNightClouds.svg",
-  "assets/mediumNightClouds.svg",
-  "assets/mediumNightClouds.svg",
-  "assets/mediumNightClouds.svg",
-  "assets/mediumNightClouds.svg",
-  "assets/mediumNightClouds.svg",
+  day,
+  day,
+  lightDayClouds,
+  mediumDayClouds,
+  mediumDayClouds,
+  heavyDayClouds,
+  heavyDayClouds,
+  heavyDayClouds,
+  heavyDayClouds,
+  lightRain,
+  rainWithSun,
+  rainWithSun,
+  thunder,
+  rainWithSun,
+  rainWithSun,
+  heavyRain,
+  lightSnow,
+  lightSnow,
+  lightSnow,
+  heavySnow,
+  snowWithSun,
+  mediumSnow,
+  mediumSnow,
+  mediumSnow,
+  mediumSnow,
+  mediumSnow,
+  mediumSnow,
+  day,
+  day,
+  lightDayClouds,
+  night,
+  lightNightClouds,
+  lightNightClouds,
+  lightNightClouds,
+  mediumNightClouds,
+  heavyNightClouds,
+  mediumNightClouds,
+  mediumNightClouds,
+  mediumNightClouds,
+  mediumNightClouds,
+  mediumNightClouds,
+  mediumNightClouds,
 ];
