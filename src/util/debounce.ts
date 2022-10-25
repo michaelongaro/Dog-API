ab81df7aae58aba4988b767d28137d46c0757242
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-25 18:49:13 -0500

    adding dist

--- src/util/debounce.ts
+++
@@ -1,9 +0,0 @@
-export default function debounce(cb: Function, delay = 1000) {
-  let timeout: number;
-  return (...args: any) => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      cb(...args);
-    }, delay);
-  };
-}
